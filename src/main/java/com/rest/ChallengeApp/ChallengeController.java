693c08cdedc04a0f1bedf4b3ba13623dc26f56d0
Isuru Indrajith <isuruindrajith680@gmail.com>
2026-05-11 15:25:09 +0530

    Added postmapping function to the challengeController

--- src/main/java/com/rest/ChallengeApp/ChallengeController.java
+++ src/main/java/com/rest/ChallengeApp/ChallengeController.java
@@ -1,6 +1,8 @@
 package com.rest.ChallengeApp;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -15,10 +17,17 @@ public class ChallengeController {
         challenges.add(challenge01);
     }
 
-
-
     @GetMapping("/challenges")
     public  List<Challenge> getAllChallenges(){
         return challenges;
     }
+
+
+    @PostMapping("/challenges")
+    public String addChallenge(@RequestBody Challenge challenge){
+        challenges.add(challenge);
+        return "Challenge Added successfully";
+    }
+
 }
+
